6c1b783ce5bd6f1add2a79fb746f57ff03ca5817
github-action-benchmark <github@users.noreply.github.com>
2026-09-06 22:50:40 +0000

    build based on b2ba52e

---
+++ previews/PR6/assets/chunks/swimlanesDiagram-VR7AAH4N.BiZCKLwq.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-HODETNUW.zhPR7VrE.js";import{_ as a}from"../app.B4I0D22P.js";import"./chunk-5VM5RSS4.BoEV3stj.js";import"./chunk-XXDRQBXY.D8XX_ld8.js";import"./chunk-POPQ4Y6H.CXRzpA_I.js";import"./chunk-F27PBJKO.DkMnLA6D.js";import"./channel.Byyhl-p2.js";import"./framework.DTgpnE-u.js";import"./theme.Dox_R7D5.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
